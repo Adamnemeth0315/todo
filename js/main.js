f655b1a3f4727e13a28d090ec66456c2e4e6bb33
Adam <adam.nemeth.0315@gmail.com>
2020-12-08 14:22:19 +0100

    Alap css rész megvan úgy ahogy

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const todoInput = document.querySelector('.input');
+const addButton = document.querySelector('.addBtn');
+const todoItems = document.querySelector('.todo__ul');
+
+(function getDate() {
+    const date = document.querySelector('.date');
+    const dateNow = new Date();
+    date.textContent = dateNow.toLocaleDateString('hu');
+})();
